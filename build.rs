2e3422d945b7a36b25f4769025569924b233d5e6
Thoren Paulson <thoren.paulson@gmail.com>
2020-05-13 20:51:45 -0700

    Initial commit.
    Adds basic project outline and a bit of code.

---
+++ build.rs
@@ -0,0 +1,16 @@
+use protobuf_codegen_pure as proto;
+
+
+
+fn main() {
+    println!("cargo:rerun-if-changed=proto/index.proto");
+    proto::run(proto::Args {
+        out_dir: "src/proto",
+        input: &["proto/index.proto"],
+        includes: &["proto"],
+        customize: proto::Customize {
+          ..Default::default()
+        },
+    }).expect("Protobuf code generation failed.");
+
+}

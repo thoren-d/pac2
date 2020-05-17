22c2878feed88d41e5ab74a992d1abbbdc7ea268
Thoren Paulson <thoren.paulson@gmail.com>
2020-05-16 22:43:01 -0700

    Fix code generation.
    Cargo won't package crates that modify code in their repo with build
    scripts, so had to move generated proto code to OUT_DIR.

--- build.rs
+++ build.rs
@@ -1,9 +1,24 @@
 use protobuf_codegen_pure as proto;
 
+use std::env;
+use std::fs;
+use std::path;
+
+const PROTO_MOD_RS: &[u8] = b"
+pub mod index;
+";
+
 fn main() {
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let out_dir = path::Path::new(&out_dir);
+
+    let proto_out_dir = path::PathBuf::from(out_dir).join("proto");
+    fs::create_dir_all(&proto_out_dir).unwrap();
+
+    println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=proto/index.proto");
     proto::run(proto::Args {
-        out_dir: "src/proto",
+        out_dir: proto_out_dir.to_str().unwrap(),
         input: &["proto/index.proto"],
         includes: &["proto"],
         customize: proto::Customize {
@@ -11,4 +26,6 @@ fn main() {
         },
     })
     .expect("Protobuf code generation failed.");
+
+    fs::write(proto_out_dir.join("mod.rs"), PROTO_MOD_RS).unwrap();
 }
